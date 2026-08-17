0ed22292fd54a24afe3a8ce3e7cd170d0b7b148d
Efren-David <202110771@tese.edu.mx>
2026-08-17 16:58:37 -0600

    Se agrega salto de linea en el controller

--- src/main/java/com/java_algorithms_api/java_algorithms_api/controller/AlgorithmsApiController.java
+++ src/main/java/com/java_algorithms_api/java_algorithms_api/controller/AlgorithmsApiController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlgorithmsApiController {
 
     
+    
     private final AlgorithmService algorithmService;
 
     public AlgorithmsApiController(AlgorithmService algorithmService) {
