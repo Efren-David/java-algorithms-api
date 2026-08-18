cdbb5e48499e42cd0a09dbe6594479cb0e6e39ba
Efren-David <202110771@tese.edu.mx>
2026-08-17 18:06:36 -0600

    ## Se incoorpora la clase mapper

---
+++ src/main/java/com/java_algorithms_api/java_algorithms_api/mapper/Mapper.java
@@ -0,0 +1,23 @@
+package com.java_algorithms_api.java_algorithms_api.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.java_algorithms_api.java_algorithms_api.model.request.ParamRequestString;
+import com.java_algorithms_api.java_algorithms_api.model.response.ParamResponseString;
+
+@Component
+public class Mapper {
+
+    public ParamResponseString toResponse(ParamRequestString request, String result) {
+
+        ParamResponseString response = new ParamResponseString();
+
+        response.setCodigo("0");
+        response.setMsj("Cadena invertida exitosamente");
+        response.setCadenaEntrada(request.getCadena());
+        response.setCadenaSalisa(result);
+
+        return response;
+    }
+
+}
